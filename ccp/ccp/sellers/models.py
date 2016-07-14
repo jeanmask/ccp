8cc3520c35562fb2fcbe93ba5635ae4e7f92bb62
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2016-07-14 16:30:06 -0300

    Create Seller model

--- ccp/ccp/sellers/models.py
+++ ccp/ccp/sellers/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class Seller(models.Model):
+    """Sellers model"""
+    name = models.CharField(
+        verbose_name=_('Name'),
+        max_length=100,
+    )
+    homepage = models.URLField(
+        verbose_name=_('Homepage'),
+        help_text=_('Seller\'s homepage URL'),
+    )
+
+    class Meta:
+        verbose_name = _('Seller')
+        verbose_name_plural = _('Sellers')
+        ordering = ('name',)
