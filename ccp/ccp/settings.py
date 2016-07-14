9e1fd306287f2f6ca423402b5fcf4b2f2388d10c
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2016-07-14 19:32:27 -0300

    Update django-money available currencies

--- ccp/ccp/settings.py
+++ ccp/ccp/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 
     # 3rd party apps
@@ -127,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Currency definitions - django-money
+CURRENCIES = ('USD', 'BRL', 'EUR',)
