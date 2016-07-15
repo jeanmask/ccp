be2cf5f4964990adaccac06870f2178868bdc668
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2016-07-15 15:37:02 -0300

    Create template struct

--- ccp/ccp/urls.py
+++ ccp/ccp/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 api_urls = [
     url(r'^', include('ccp.sellers.api_urls', namespace='api_sellers')),
@@ -10,6 +11,7 @@ api_urls = [
 ]
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="ccp/home.html")),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(api_urls)),
 ]
