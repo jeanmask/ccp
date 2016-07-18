a2161445caa288b6ee1a4f92ec53630c0b8bebb3
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2016-07-18 04:33:49 -0300

    Add compare-cloud.herokuapp.com domain on ALLOWED_HOSTS

--- src/ccp/settings.py
+++ src/ccp/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '::1', '.jean.bz']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '::1', '.jean.bz',
+                 'compare-cloud.herokuapp.com']
 
 
 # Application definition
