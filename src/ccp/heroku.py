d901d9346bbe912cb50186e821be854b4bf9c4a4
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2016-07-19 14:50:54 -0300

    Fix ENVIRONMENT envvar name

--- src/ccp/heroku.py
+++ src/ccp/heroku.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 from .settings import *
 
-DEBUG = TEMPLATE_DEBUG = bool(os.environ.get('ENVIROMENT') != 'production')
+DEBUG = TEMPLATE_DEBUG = bool(os.environ.get('ENVIRONMENT') != 'production')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
