698dbafe018f15e14e76f7a1b363e2e0b7e4a688
Jean O. Rodrigues <jeanmaskpl@gmail.com>
2016-07-20 21:33:31 -0300

    Improve urls namming

--- src/ccp/urls.py
+++ src/ccp/urls.py
@@ -6,13 +6,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 api_urls = [
-    url(r'^', include('ccp.sellers.api_urls', namespace='api_sellers')),
-    url(r'^', include('ccp.offers.api_urls', namespace='api_offers')),
+    url(r'^', include('ccp.sellers.api_urls')),
+    url(r'^', include('ccp.offers.api_urls')),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 urlpatterns = [
     # url(r'^$', TemplateView.as_view(template_name="ccp/home.html")),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(api_urls)),
+    url(r'^api/', include(api_urls, namespace='api')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
